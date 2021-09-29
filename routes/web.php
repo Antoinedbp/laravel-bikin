d7eac8b001d3c69e85ffe31bbaee2c9f6893e524
Antoinedbp <antoinedebassompierre@hotmail.com>
2021-09-29 16:20:22 +0100

    commit 8

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,17 @@
 <?php
 
+use App\Http\Controllers\NavbarController;
+use App\Http\Controllers\TitreController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\DeuxiemeSectionController;
+use App\Http\Controllers\FeatureController;
+use App\Http\Controllers\FooterController;
+use App\Http\Controllers\PortfolioController;
+use App\Http\Controllers\PremiereSectionController;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\TeamController;
+use App\Http\Controllers\TestimonialController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +28,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/backoffice',[BackController::class, 'index'])->name('hombo');
+
+Route::resource('/titres', TitreController::class);
+Route::resource('/headers', NavbarController::class);
+Route::resource('/heros', PremiereSectionController::class);
+Route::resource('/abouts', DeuxiemeSectionController::class);
+Route::resource('/features', FeatureController::class);
+Route::resource('/services', ServiceController::class);
+Route::resource('/portfolios', PortfolioController::class);
+Route::resource('/testimonials', TestimonialController::class);
+Route::resource('/teams', TeamController::class);
+Route::resource('/contacts', ContactController::class);
+Route::resource('/footers', FooterController::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
