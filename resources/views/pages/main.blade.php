f8a6ddff664fa749125f46e2f10ad6c33c949fd2
Antoinedbp <antoinedebassompierre@hotmail.com>
2021-09-29 10:02:23 +0100

    commit 3

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -2,11 +2,13 @@
 
 @section('content')
     @include('partials.heroSec')
-    @include('partials.aboutSec')
-    @include('partials.featSec')
-    @include('partials.servicesSec')
-    @include('partials.portfolioSec')
-    @include('partials.testimonialsSec')
-    @include('partials.teamSec')
-    @include('partials.contactSec')
+    <main id="main">
+        @include('partials.aboutSec')
+        @include('partials.featSec')
+        @include('partials.servicesSec')
+        @include('partials.portfolioSec')
+        @include('partials.testimonialsSec')
+        @include('partials.teamSec')
+        @include('partials.contactSec')
+    </main>
 @endsection
