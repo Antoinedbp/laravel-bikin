e83ad2828ee36392c2187da90f60f98e0e60449d
Antoinedbp <antoinedebassompierre@hotmail.com>
2021-09-29 09:44:07 +0100

    commit 2

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -1,5 +1,12 @@
 @extends('template.welcome')
 
 @section('content')
-    
+    @include('partials.heroSec')
+    @include('partials.aboutSec')
+    @include('partials.featSec')
+    @include('partials.servicesSec')
+    @include('partials.portfolioSec')
+    @include('partials.testimonialsSec')
+    @include('partials.teamSec')
+    @include('partials.contactSec')
 @endsection
