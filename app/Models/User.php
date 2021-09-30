43dc2a938dabd2bf7ebb362ca54372f35e5a7401
Antoinedbp <antoinedebassompierre@hotmail.com>
2021-09-30 14:25:30 +0100

    commit protected

--- app/Models/User.php
+++ app/Models/User.php
@@ -17,10 +17,13 @@ class User extends Authenticatable
      *
      * @var string[]
      */
+
+     protected $table = 'users';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+    'name',
+    'email',
+    'password',
     ];
 
     /**
